531e39a8fa83b645d1b3c19140f6d5128b60df4f
Junjia Huang <junjia.huang@smartx.com>
2022-12-28 16:33:14 +0800

    update react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: './',
+    createDefaultProgram: true,
+  },
+  plugins: ['@typescript-eslint', 'import'],
 };
